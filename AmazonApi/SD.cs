eb4a8b6c9498a8bd46aabf23cbea46498a59694a
kobi abitbul <kabitbul@gmail.com>
2025-03-03 09:41:23 +0200

    03/03/2025

--- AmazonApi/SD.cs
+++ AmazonApi/SD.cs
@@ -5,16 +5,13 @@ namespace AmazonAPI
     public class SD
     {
         public static string connectionStr = Environment.GetEnvironmentVariable("KT_CON_STR");
-       // public static string clientSecret = Environment.GetEnvironmentVariable("AMZ_CLIENT_SECRET");
-   //     public static string clientID = Environment.GetEnvironmentVariable("AMZ_CLIENT_ID");
-   //     public static string appID = "amzn1.sp.solution.146960c7-528f-41ad-b599-332211d5db0c";//"amzn1.sp.solution.330efd4c-70f0-45a1-97da-6eecf971fa8e";//Environment.GetEnvironmentVariable("AMZ_APP_ID");
-//        public static string refreshToken = "Atzr|IwEBIHBr5E2JVOBUcJn6oYRUHYYJqLuthDTXdXseHeZEf96STS-kxaxORBZbKCN-Z2B-tC9LHZzgqH5csqkqYNDaUhxdI-S7okkW2AvtDRLHv3T6Z7KV2cLzVeCY53SOygItS8EO1e0e-l3fRJ_ifnDxQG0quT6Sot52dYQJjFWjfkQPEhhcUGN8X2uqXtGgPrcvBRC51QkxBmrHr0lnPu3QIVDNEKY2HZmLZ6oeFFDXl_3UP_sDeLRgIa5aBMRnXFHs6kLKvyKJmzf0xdUANQqsXuA5rI83NF4RnK__eb7xh8psqsimoJHrcO6JEshTDRgPPJLqvLjJs_uDaAHf2XRtBCk4";//Environment.GetEnvironmentVariable("AMZ_REFRESH_TOKEN");
+        public static string clientSecret = Environment.GetEnvironmentVariable("AMZ_CLIENT_SECRET");
+       public static string clientID = Environment.GetEnvironmentVariable("AMZ_CLIENT_ID");
+       public static string appID = Environment.GetEnvironmentVariable("AMZ_APP_ID");
+      public static string refreshToken = Environment.GetEnvironmentVariable("AMZ_REFRESH_TOKEN");
         public static string USMarketplace = "ATVPDKIKX0DER";
         public static string CAMarketplace = "A2EUQ1WTGCTBG2";
-public static string clientSecret = "amzn1.oa2-cs.v1.93131c76ec140601ef7eacffe9e859e451cedfada89fe8ea9e7cfeaf026792d1";
-        public static string clientID = "amzn1.application-oa2-client.8607a8ecf2a14446b8b979a33a83a69b";
-        public static string appID = "amzn1.sp.solution.146960c7-528f-41ad-b599-332211d5db0c";
-        public static string refreshToken = "Atzr|IwEBIChqofwjmxsc1mP0RzO6DM7izCc_KwWZa_OdpVueYs9OyS-w5vYSUz5FdG4QRALBYzeZ1R5CbILP6eAEHgh8gF8A6Wak8lnwz4jWgcjvbkl-qCX2NXycBwGfMEEb7athexHoNKWSdgl8KBMuXLDV9J3eaxecVEqXd1OWlmFRXAscaZ1vY0q2qqk0FWGcVDUFEC9Rryb5bciQdho2MFUnlVG2gLs-wmpDXX_I6pglTHRoQCLPNo8QpffziNXvLEBJSYFynQYo97s-16ooRrD5CADPzsFoOcIjWfKOWSrVZ0wJjaz65EmMYzhdhsrutPwfYMO0P_cCJk0rKqgZTidACcpI";
+
        
 
     }
